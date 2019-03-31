4e516ea7c4628153d7e0346d815d53f94ce33930
dnsl48 <dnsl48@gmail.com>
2019-04-01 06:36:26 +1300

    CircleCI / fixing the tests

--- tests/examples/mod.rs
+++ tests/examples/mod.rs
@@ -1,12 +1,17 @@
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_reader"))))]
-pub mod reader;
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_savant"))))]
-pub mod savant;
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_sage"))))]
-pub mod sage;
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_book"))))]
+#[cfg (all (test, feature = "test_book"))]
 pub mod book;
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_orchestra"))))]
-pub mod orchestra;
-#[cfg (all (test, any ( not (feature = "onetest"), all (feature = "onetest", feature = "test_face"))))]
+
+#[cfg (all (test, feature = "test_face"))]
 pub mod face;
+
+#[cfg (all (test, feature = "test_orchestra"))]
+pub mod orchestra;
+
+#[cfg (all (test, feature = "test_reader"))]
+pub mod reader;
+
+#[cfg (all (test, feature = "test_sage"))]
+pub mod sage;
+
+#[cfg (all (test, feature = "test_savant"))]
+pub mod savant;
